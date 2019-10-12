8856dc08bd1ead0edc55a5a185c5b84de9ab4fc2
Vechro <stenmr@gmail.com>
2019-10-12 21:23:34 +0300

    Add moment

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const fs = require("fs");
 const jsonfile = require("jsonfile");
 const Discord = require("discord.js");
 const roman = require("romanjs");
+const moment = require("moment");
 const { prefix, token, path } = require("./config.json");
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
-// let storage;
-
-// Example structure of the JSON
+// Original structure of the JSON
 const data = {
     counting: true,
     channelId: "",
@@ -30,27 +29,12 @@ function InitializeStorage(storage) {
     storage.users = new Map(storage.users);
 
     storage.users.toJSON = function () {
-        return Object.entries(storage.users);
+        return [...storage.users.entries()]
     };
 
     return storage;
 }
-/*
-if (fs.existsSync(path)) {
-    storage = jsonfile.readFileSync(path, function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-} else {
-    storage = data;
-    jsonfile.writeFileSync(path, storage, function (err) {
-        if (err) {
-            console.log(err);
-        }
-    });
-}
-*/
+
 function isValidInt(string, expectedInt) {
     if (parseInt(string, 10) === expectedInt) {
         return true;
@@ -74,7 +58,7 @@ for (const file of commandFiles) {
 client.once("ready", () => {
     console.log("Ready!");
 });
-// Make commands admin-only, (TODO:) resetting the count on misuse
+// (TODO:) resetting the count on command misuse
 client.on("message", message => {
 
     const args = message.content.slice(prefix.length).split(/ +/);
@@ -83,18 +67,29 @@ client.on("message", message => {
     try {
         if (message.channel.id == storage.channelId && !message.content.startsWith(prefix) && !message.author.bot) {
             if (isValidInt(countAttempt, storage.lastNumber + 1)) {
+                console.log("pass " + countAttempt);
                 storage.lastNumber++;
                 jsonfile.writeFileSync(path, storage);
                 return;
             } else {
                 if (storage.users.has(message.member)) {
+                    console.log("test1");
+                    /*
                     const banishments = storage.users[message.member].get("banishments");
                     storage.users[message.member].set("banishments", banishments + 1);
-
-                    // storage.users[message.member].set("unbanDate", )
+                    */
+                    const user = storage.users.get(message.member);
+                    user.banishments += 1;
+                    user.unbanDate = moment().add(Math.sqrt(storage.lastNumber) * 0.1 + user.banishments, "days"),
+                    storage.users.set(user);
 
                 } else {
-                    storage.users[message.member].set("banishments", 1);
+                    console.log("test2");
+                    // storage.users[message.member].set("banishments", 1);
+                    storage.users.set(message.member, {
+                        banishments: 1,
+                        unbanDate: moment().add(Math.sqrt(storage.lastNumber) * 0.1 + 1, "days"),
+                    });
                 }
                 message.channel.send(`${message.member} messed up.`);
                 storage.lastNumber = Math.floor(storage.lastNumber * 0.9);

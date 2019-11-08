c8919802e801756e63b047015f901927370d4a6f
vechro <stenmr@gmail.com>
2019-11-08 10:28:17 +0200

    Remove unused cooldown

--- commands/help.js
+++ commands/help.js
@@ -5,7 +5,6 @@ module.exports = {
     description: "List all of my commands or info about a specific command.",
     aliases: ["commands"],
     usage: "[command name]",
-    cooldown: 5,
     execute(message, args) {
         const data = [];
         const { commands } = message.client;

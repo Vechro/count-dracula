29ab3e561879c96b7de9d8d08b3d56a1b5a5a589
Vechro <stenmr@gmail.com>
2019-12-29 21:17:44 +0200

    Fix typo

--- commands/help.js
+++ commands/help.js
@@ -21,7 +21,7 @@ module.exports = {
         const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
         if (!command) {
-            return message.reply("that's not a valid command!");
+            return message.reply("That's not a valid command!");
         }
 
         data.push(`**Name:** ${command.name}`);

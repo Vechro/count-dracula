8ecad910d7dae680adde613d3a1fc2103bf1ca95
Vechro <stenmr@gmail.com>
2020-01-01 21:48:24 +0200

    Fix unrestrict

--- commands/unrestrict.js
+++ commands/unrestrict.js
@@ -1,18 +1,18 @@
 const jsonfile = require("jsonfile");
 const { path } = require("../config.json");
-const { unrestrictUser } = require("../functions");
+const { setUserRestriction } = require("../functions");
 
 module.exports = {
     name: "unrestrict",
     description: "Unrestrict user from counting.",
     aliases: ["unban"],
     execute(message, args, storage) {
-        unrestrictUser(message.client, message.guild.id, storage.channelId, message.mentions.users.first().id);
+        setUserRestriction(message.client, message.guild.id, storage.channelId, message.mentions.users.first().id, null);
 
         const user = storage.users.get(message.mentions.users.first().id);
         user.unbanDate = 0;
 
         jsonfile.writeFileSync(path, storage);
-        message.reply("unbanned!");
+        message.channel.send("User has been unbanned!");
     },
 };

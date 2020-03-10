67191bb2937cb1678cdc8a37b0ef55c80a485bc8
vechro <stenmr@gmail.com>
2020-03-10 16:48:26 +0200

    Add command for changing rules

---
+++ commands/setrule.js
@@ -0,0 +1,36 @@
+const jsonfile = require("jsonfile");
+
+module.exports = {
+    name: "setrule",
+    description: "Change a rule",
+    aliases: ["rule"],
+    usage: "<rule name> <true | false>",
+    execute(message, args, storage) {
+
+        const ruleName = args[0];
+        const ruleStatus = args[1];
+        let derivedBoolean;
+
+        if (ruleStatus.toLowerCase() == "true") {
+            derivedBoolean = true;
+        } else if (ruleStatus.toLowerCase() == "false") {
+            derivedBoolean = false;
+        } else {
+            message.channel.send("Unable to interpret rule state");
+            return;
+        }
+        
+
+        const knownRules = Object.keys(storage.rules);
+
+        if (knownRules.includes(ruleName)) {
+            storage.rules[ruleName] = derivedBoolean;
+        } else {
+            message.channel.send("Unable to find such rule");
+            return;
+        }
+
+        jsonfile.writeFile(process.env.DATA_PATH, storage);
+        message.channel.send("Rule changed");
+    },
+};
